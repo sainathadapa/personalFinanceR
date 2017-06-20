694a6ce2cff84600247128abdeab641b86b9f50e
Sainath Adapa <adapasainath@gmail.com>
2017-06-20 19:01:51 +0530

    Added attribution

--- R/xirr.R
+++ R/xirr.R
@@ -1,21 +1,9 @@
-#~ Title: XIRR Excel function simulation
-#~ 
-#~ Reference 1: XIRR manual - http://office.microsoft.com/en-gb/excel-help/xirr-HP005209341.aspx
-#~ Reference 2: How to calculate IRR manually - http://www.s-anand.net/Calculating_IRR_manually.html
-#~ 
-#~ Step 1: enter zeroes (0) against dates that do not have any cash outflow or inflows.
-#~ Step 1bis: calculate IRR for these cash flow values using normal IRR function.
-#~ Step 1tris: or using an iteractive approach as bisection method to find the NPV zeroes.
-#~ Step 2: multiply this value of IRR by 365 to get annual IRR (since, these are daily cash flows).
-#~ Step 3: refine using the formula =( 1+ R / 365) ^ 365 - 1), where R is the the value obtained in Step2.
-#~
-
-# 
+# Modified from original code by Alberto Santini
+# Original code is present at https://gist.github.com/albertosantini/3638454
 sppv <- function (i, n) {
   return((1 + i/100)^(-n))
 }
 
-# Net Present Value
 npv <- function(x, i) {
   npv = c()
   for (k in 1:length(i)) {
@@ -66,15 +54,3 @@ xirr <- function (cashflow, dates) {
   
   irr
 }
-
-#~ npv(rep(8792,12), 666.31/12) # 15755.01 - 100000 - 10.0088%
-
-# the correct IRR is 10.06% - source: matlab example
-mycashflow <- c(-10000,2500, 2000, 3000,4000)
-mydates <- c("12-01-87", "14-02-88", "03-03-88", "14-06-88", "01-12-88")
-xirr(mycashflow, mydates)
-
-# the correct IRR is 37.34% - source: microsoft example
-mycashflow <- c(-10000,2750,4250,3250,2750)
-mydates <- c("1-01-08","1-03-08","30-10-08","15-02-09","1-04-09")
-xirr(mycashflow, mydates)
